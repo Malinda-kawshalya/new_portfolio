71c78067a59e36c83ba4e36d88d2219b2351c1a2
Malinda Kawshalya <malindakawshalya@gmail.com>
2025-06-19 21:44:18 +0530

    updzte

--- portfolio/src/lib/data/projects.js
+++ portfolio/src/lib/data/projects.js
@@ -3,18 +3,18 @@ export const projects = [
     id: 1,
     title: "Galaxy Explorer",
     description: "Interactive 3D visualization of galaxies with WebGL and Three.js, featuring realistic star systems and planet exploration.",
-    image: "/images/projects/galaxy-explorer.jpg",
+    image: "/images/galaxy.png",
     link: "/projects/galaxy-explorer",
     tech: ["React", "Three.js", "WebGL"]
   },
-  {
-    id: 2,
-    title: "Event Guru",
-    description: "Ticketing platform for event browsing, booking, and organizer management",
-    image: "/images/projects/cosmic-dashboard.jpg",
-    link: "/projects/cosmic-dashboard",
-    tech: ["Node.js", "React.js", "Express.js", "MongoDB"]
-  },
+{
+  id: 2,
+  title: "Event Guru",
+  description: "Ticketing platform for event browsing, booking, and organizer management",
+  image: "/images/eventguru.png",
+  link: "/projects/cosmic-dashboard",
+  tech: ["Node.js", "React.js", "Express.js", "MongoDB"]
+},
   {
     id: 3,
     title: "Antiqua",
@@ -25,9 +25,9 @@ export const projects = [
   },
   {
     id: 4,
-    title: "Shopping Site",
+    title: "Fashion Fusion",
     description: "E-commerce platform with user authentication, admin panel, and responsive design.",
-    image: "/images/projects/orbital-commerce.jpg",
+    image: "/images/fashion_fusion.png",
     link: "/projects/orbital-commerce",
     tech: ["JAVA", "HTML", "Css", "JavaScript","Tomcat"]
   },
@@ -35,7 +35,7 @@ export const projects = [
     id: 5,
     title: " LMS",
     description: "Academic tool for managing assignments, grading, and user roles.",
-    image: "/images/projects/spacex-tracker.jpg",
+    image: "/images/lms.png",
     link: "/projects/spacex-tracker",
     tech: ["HTML", "PHP", "SQL", "JavaScript"]
   },
@@ -43,7 +43,7 @@ export const projects = [
     id: 6,
     title: "BARZA APP",
     description: "Barter app with Star Points system for cashless item and service exchange",
-    image: "/images/projects/astro-weather.jpg",
+    image: "/images/barza.jpg",
     link: "/projects/astro-weather",
     tech: ["Flutter", "Firebase", ]
   }
